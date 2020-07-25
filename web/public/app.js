13c95614ce49bd0881e503ee0ec66bb59427d12d
Jake Roussis <jroussis@deakin.edu.au>
2020-07-26 02:43:53 +1000

    NEW: ADD Node.js back-end web server

--- web/public/app.js
+++ web/public/app.js
@@ -14,7 +14,7 @@ $('#add-device').on('click', function () {
     const name = $('#name').val();
     devices.push({ user, name });
     localStorage.setItem('devices', JSON.stringify(devices));
-    location.href = 'device-list.html';
+    location.href = '/';
 });
 
 $('#send-command').on('click', function () {
