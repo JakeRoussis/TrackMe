19579446c3853d24e7d1eb48839526ccffc1940c
Jake Roussis <jroussis@deakin.edu.au>
2020-08-01 22:12:08 +1000

    ENHANCE: Improve security/login functionality

--- api/api.js
+++ api/api.js
@@ -8,10 +8,17 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+
 const Device = require('./models/device');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.post('/api/devices', (req, res) => {
     const { name, user, sensorData } = req.body;
@@ -75,16 +82,75 @@ app.get('/api/devices', (req, res) => {
     });
 });
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 app.post('/api/send-command', (req, res) => {
     console.log(req.body);
 });
 
+app.post('/api/authenticate', (req, res) => {
+    const { name, password } = req.body;
+    User.findOne({ "name":name }, (err, found) => {
+        if (err) {
+            return res.send(err);
+        }
+        else if (!found) {
+            return res.send("User does not exist!");
+        }
+        else if (found.password != password) {
+            return res.send("Incorrect Password!");
+        }
+        else {
+            return res.json({
+                success: true,
+                message: 'Authenticated successfully',
+                isAdmin: found.isAdmin
+            });
+        }
+    });
+});
+
+app.post('/api/registration', (req, res) => {
+    const { name, password, isAdmin } = req.body;
+    User.findOne({"name":name}, (err, found) => {
+        if (err) {
+            return res.send(err);
+        }
+        else if (found) {
+            res.send("User already exists!");
+        }
+        else {
+            const newUser = new User({
+                name: name,
+                password,
+                isAdmin
+            });
+            newUser.save(err => {
+                return err ? res.send(err) : res.json({
+                    success: true,
+                    message: 'Created new user'
+                });
+            });
+        }
+    });
+});
+
+app.get('/api/devices/:deviceId/device-history', (req, res) => {
+    const { deviceId } = req.params;
+    Device.findOne({ "_id": deviceId }, (err, devices) => {
+        const { sensorData } = devices;
+        return err
+            ? res.send(err)
+            : res.send(sensorData);
+    });
+});
+
+app.get('/api/users/:user/devices', (req, res) => {
+    const { user } = req.params;
+    Device.find({ "user": user }, (err, devices) => {
+        return err
+            ? res.send(err)
+            : res.send(devices);
+    });
+});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
--- web/public/app.js
+++ web/public/app.js
@@ -1,10 +1,52 @@
 $("#navbar").load("navbar.html");
 $("#footer").load("footer.html");
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = "http://localhost:5000/api";
 const response = $.get("http://localhost:3001/devices");
+const currentUser = localStorage.getItem("user");
 console.log(response);
+/*
 const users = JSON.parse(localStorage.getItem("users")) || [];
+*/
+
+if (currentUser) {
+    $.get(`${API_URL}/users/${currentUser}/devices`)
+        .then(response => {
+            response.forEach((device) => {
+                $("#devices tbody").append(`
+                <tr data-device-id=${device._id}>
+                    <td>${device.user}</td>
+                    <td>${device.name}</td>
+                </tr>`
+                );
+            });
+            $('#devices tbody tr').on('click', (e) => {
+                const deviceId = e.currentTarget.getAttribute('data-device-id');
+                $.get(`${API_URL}/devices/${deviceId}/device-history`)
+                .then(response => {
+                    response.map(sensorData => {
+                        $('#historyContent').append(`
+                        <tr>
+                            <td>${sensorData.ts}</td>
+                            <td>${sensorData.temp}</td>
+                            <td>${sensorData.loc.lat}</td>
+                            <td>${sensorData.loc.lon}</td>
+                        </tr>
+                    `);
+                    });
+                    $('#historyModal').modal('show');
+                });
+            });
+        })
+        .catch((error) => {
+            console.error(`Error: ${error}`);
+        });
+} else {
+    const path = window.location.pathname;
+    if (path !== "/login" && path !== "/registration") {
+        location.href = "/login";
+    }
+}
 
 $.get(`${API_URL}/devices`)
     .then((response) => {
@@ -14,6 +56,14 @@ $.get(`${API_URL}/devices`)
                 <td>${device.user}</td>
                 <td>${device.name}</td>
             </tr>`);
+        });
+        $("#devices tbody tr").on("click", (e) => {
+            const deviceId = e.currentTarget.getAttribute("data-device-id");
+            $.get(`${API_URL}/devices/${deviceId}/device-history`).then(
+                (response) => {
+                    console.log(response);
+                }
+            );
         });
     })
     .catch((error) => {
@@ -30,7 +80,7 @@ $("#add-device").on("click", () => {
         user,
         sensorData,
     };
-    
+
     $.post(`${API_URL}/devices`, body)
         .then((response) => {
             location.href = "/";
@@ -53,37 +103,43 @@ $("#register").on("click", function () {
     const exists = users.find((user) => user.name === username);
     console.log(users);
 
-    if (!exists && password === confirm) {
-        users.push({ name: username, password });
-        localStorage.setItem("users", JSON.stringify(users));
-        location.href = "/login";
-    } else {
+    if (exists || password != confirm) {
         $("#message").append(
             '<div class="alert alert-danger alert-dismissible fade show" role="alert">Username already taken or passwords do not match! \
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
         );
+    } else {
+        $.post(`${API_URL}/registration`, { name, password }).then((response) => {
+            if (response.success) {
+                location.href = '/login';
+            } else {
+                $('#alert').append(`<p class="alert alert-danger">${response}</p>`); 
+            }
+        });
     }
 });
 
 $("#login").on("click", function () {
     const username = $("#username").val();
     const password = $("#password").val();
-    const userExists = users.find(
-        (user) => user.name === username && user.password === password
-    );
+    $.post(`${API_URL}/authenticate`, { "name":username, "password":password }).then((response) => {
+        if (response.success) {
+            localStorage.setItem("user", username);
+            localStorage.setItem("isAdmin", response.isAdmin);
+            localStorage.setItem("isAuthenticated", true);
 
-    if (userExists) {
-        localStorage.setItem("isAuthenticated", true);
-        location.href = "/";
-    } else {
-        $("#message").append(
-            '<div class="alert alert-danger alert-dismissible fade show" role="alert">Incorrect username or password! \
+            location.href = "/";
+        } else {
+            $("#message").append(
+                '<div class="alert alert-danger alert-dismissible fade show" role="alert">Incorrect username or password! \
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
-        );
-    }
+            );
+        }
+    });
 });
 
 const logout = () => {
+    localStorage.removeItem("user");
     localStorage.removeItem("isAuthenticated");
     location.href = "/login";
 };
