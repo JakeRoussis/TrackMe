85b9e7e017b29d00768f8412b0a6a978a106a108
Jake Roussis <jroussis@deakin.edu.au>
2020-08-16 17:06:26 +1000

    ENHANCE: Add data addition via MQTT communication feature

--- mqtt/mqtt.js
+++ mqtt/mqtt.js
@@ -1,8 +1,17 @@
 const mqtt = require('mqtt');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const Device = require('../api/models/device');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://Jake:sit209db@cluster0.nf7wt.mongodb.net/test')
+
 const app = express();
 const port = process.env.PORT || 5001;
+
+
+const rand = require('random-int');
+const randomCoordinates= require('random-coordinates');
 
 app.use(express.static('public'));
 app.use((req, res, next) => {
@@ -18,13 +27,52 @@ app.use(bodyParser.urlencoded({
 
 const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
 client.on('connect', () => {
+    client.subscribe('/sensorData');
     console.log('mqtt connected');
 });
 
+client.on('message', (topic, message) => {
+    if (topic == '/sensorData') {
+        const data = JSON.parse(message);
+
+        Device.findOne({ "name": data.deviceId }, (err, device) => {
+            if (err) {
+                console.log(err)
+            }
+
+            const { sensorData } = device;
+            const { ts, loc, temp } = data;
+            sensorData.push({ ts, loc, temp });
+            device.sensorData = sensorData;
+            device.save(err => {
+                if (err) {
+                    console.log(err)
+                }
+            });
+        });
+    }
+});
+
+
 app.post('/send-command', (req, res) => {
     const { deviceId, command } = req.body;
-    const topic = `/218244867/#`;
+    const topic = `/218244867/command/${deviceId}`;
     client.publish(topic, command, () => {
+        res.send('published new message');
+    });
+});
+
+app.put('/sensor-data', (req, res) => {
+    const { deviceId } = req.body;
+    const [lat, lon] = randomCoordinates().split(", ");
+    const ts = new Date().getTime();
+    const loc = { lat, lon };
+    const temp = rand(20, 50);
+    const topic = `/sensorData`;
+    const message = JSON.stringify({ deviceId, ts, loc, temp });
+    client.publish(topic, message, () => {
+        console.log(lat, lon);
+        console.log(deviceId);
         res.send('published new message');
     });
 });
